88abf92ab860fac21170ed9ec3d2c829542bcc34
ALuciaRodas <lu.gio2904@gmail.com>
2021-07-29 19:46:31 +0000

    correccion boton create

--- odoo_biblioteca/models/book.py
+++ odoo_biblioteca/models/book.py
@@ -26,13 +26,11 @@ class Book(models.Model):
     
     @api.onchange('isbn')
     def _onchange_len_isbn(self):
-        if not self.isbn:
-            if len(self.isbn)>13:
-                raise ValidationError("El ISBN no puede tener más de 13 caracteres")
+        if len(self.isbn)>13:
+            raise ValidationError("El ISBN no puede tener más de 13 caracteres")
 
             
-    @api.constrains('isbn')
-    def _check_value_isbn(self):
-        if not self.isbn:
-            if len(self.isbn)>13:
-                raise ValidationError("El ISBN no puede tener más de 13 caracteres")
+#    @api.constrains('isbn')
+#    def _check_value_isbn(self):
+#        if len(self.isbn)>13:
+#            raise ValidationError("El ISBN no puede tener más de 13 caracteres")
