4e894b3b3f51580c0318611b454ba42cb3e84fc9
ALuciaRodas <lu.gio2904@gmail.com>
2021-08-05 19:43:36 +0000

    dia ocho

--- odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields, api
 from datetime import timedelta
 
@@ -28,6 +29,17 @@ class Session(models.Model):
                            compute='_compute_end_date',
                            inverse='_inverse_end_date',
                            store=True)
+    
+    state = fields.Selection(string='States',
+                             selection=[('draft','Draft'),
+                                        ('open','In Progress'),
+                                        ('done','Done'),
+                                        ('canceled','Canceled')],
+                             default='draft',
+                             required=True)
+    
+    total_price = fields.Float(string='Total Price',
+                              related='course_id.total_price')
     
     @api.depends('start_date', 'duration')
     def _compute_end_date(self):
