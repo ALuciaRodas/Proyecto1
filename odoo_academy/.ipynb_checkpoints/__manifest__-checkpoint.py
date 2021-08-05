66b5e8404acba3fafeba66d9b4d43a0104ea5896
ALuciaRodas <lu.gio2904@gmail.com>
2021-08-05 03:38:05 +0000

    prueba

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,6 +20,9 @@
     'depends': ['base'],
     
     'data': [
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
         'security/academy_security.xml',
         'security/ir.model.access.csv',
     ],
