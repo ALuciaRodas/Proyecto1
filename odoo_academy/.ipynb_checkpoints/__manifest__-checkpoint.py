20008324d0a67dfe33879d48e72cf20386f68b43
ALuciaRodas <lu.gio2904@gmail.com>
2021-08-05 00:44:31 +0000

    prueba

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """App para gestion de cursos""",
+    'description': """
+        Modulo para administrar:
+        -Cursos
+        -Estudiantes
+        -Pofesroes
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
