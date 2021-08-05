14764b0e3fba13e3c8671d464ccab3c4869f4568
ALuciaRodas <lu.gio2904@gmail.com>
2021-08-05 01:33:35 +0000

    prueba

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -4,3 +4,14 @@ class Course(models.Model):
     
     _name = 'academy.course'
     _description = 'Curso Info'
+    
+    name = fields.Char(string='Title', required=True)
+    
+    description = fields.Char(string='Description')
+    
+    level = fields.Selection(string='Level', 
+                        selection=[('beginner','Beginner'),
+                                   ('intermediate','Intermediate'),
+                                   ('advanced','Advanced')],
+                       copy=False)
+    active = fields.Boolean(string='Active', default=True)
