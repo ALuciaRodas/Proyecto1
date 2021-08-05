5eaa1d3857a58bdb759e42882fe2b1b7d9819970
ALuciaRodas <lu.gio2904@gmail.com>
2021-08-05 01:10:43 +0000

    prueba

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -2,5 +2,5 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
 class Course(models.Model):
     
-    _name = 'academy.curso'
+    _name = 'academy.course'
     _description = 'Curso Info'
